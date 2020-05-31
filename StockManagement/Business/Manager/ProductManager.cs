2e43786a085138d074c717f9067033657c500725
Jonathan Vega <jonathan.vega@outlook.com>
2020-05-31 13:22:16 -0500

    Added interface and implementation of Product manager.

---
+++ StockManagement/Business/Manager/ProductManager.cs
@@ -0,0 +1,20 @@
+﻿using StockManagement.Business.IManager;
+using StockManagement.Data.Entities;
+using StockManagement.Data.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockManagement.Business.Manager
+{
+    public class ProductManager : IProductManager
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductManager(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+    }
+}
