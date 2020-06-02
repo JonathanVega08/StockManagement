9b14dd38dc07e49d10f2406bd8065166d6fd8ab8
Jonathan Vega <jonathan.vega@outlook.com>
2020-06-01 19:54:32 -0500

    Added product model

---
+++ StockManagement/Models/ProductModel.cs
@@ -0,0 +1,12 @@
+﻿namespace StockManagement.Models
+{
+    public class ProductModel
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int? AgeRestriction { get; set; }
+        public string Company { get; set; }
+        public float Price { get; set; }
+    }
+}
